6fbd8c6817fc98c735bd54fb3042fff03e1b8896
erivanstdio <erivanstdio@gmail.com>
2021-10-20 19:57:42 -0300

    setando o ambiente

---
+++ src/scenes/SingIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const SingIn: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default SingIn;
